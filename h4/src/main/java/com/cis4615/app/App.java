3f58d3056041f69c228fd69719aa4e4b7a3ffe6e
ChasTest <lckshare0@gmail.com>
2019-12-10 16:44:49 -0500

    finished

---
+++ h4/src/main/java/com/cis4615/app/App.java
@@ -0,0 +1,16 @@
+package com.cis4615.app;
+import java.io.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main( String[] args )
+	{
+		try {
+			System.out.println("2147483647 + 1 looks like: " + (2147483647 + 1));
+		} catch (Exception e) {}
+	}
+}
